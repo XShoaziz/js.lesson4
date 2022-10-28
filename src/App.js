e71a62e406b67c0959b3a95d49e42a8cfb6b266f
XShoaziz <xusanovshoaziz@gmail.com>
2022-10-28 20:58:20 +0500

    update28-2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class extends Component {
   render() {
-    return <div>Salomm1 salam</div>;
+    return <div>Salomm1 salam1</div>;
   }
 }
